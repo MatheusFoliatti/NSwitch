0ba91cd751368f08511827e485e7088c38a1acf3
Matheus Folitti <matheusfoliatti@gmail.com>
2023-04-16 16:47:20 -0300

    work done

--- components/Conteudo/index.js
+++ components/Conteudo/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import { View, Text, Image, StyleSheet } from "react-native"
+import { View, Text, Image } from "react-native"
 
 import Jogos from '../../assets/smash.png'
 import stylesConteudo from "./styles";
 
 export default function Conteudo() {
     return(
-        <View> 
-            <Text> Os principais jogos estão aqui </Text>
+        <View style={ stylesConteudo.containerContent }> 
+            <Text style={ stylesConteudo.title }> Os principais jogos estão aqui </Text>
             <Image style={ stylesConteudo.contentImg } source= {Jogos} />
-            <Text> A familia Nintendo Switch é o lar de jogos exclusivos da séries como Super Smash Bros, The Legend of Zelda, Mario Kart e muitas outras </Text>
+            <Text style={ stylesConteudo.text }> A familia Nintendo Switch é o lar de jogos exclusivos da séries como Super Smash Bros, The Legend of Zelda, Mario Kart e muitas outras </Text>
         </View>
     )
 }
--- components/Conteudo/styles.js
+++ components/Conteudo/styles.js
@@ -2,12 +2,32 @@ import { StyleSheet } from "react-native";
 
 const stylesConteudo = StyleSheet.create({
     contentImg: {
-        width:'250px',
-        height:'250px',
+        width:'225px',
+        height:'150px',
         justifyContent:'center',
         display:'flex',
         alignItems:'center'
     },
+    containerContent: {
+        width: '100%',
+        justifyContent:'center',
+        alignItems:'center',
+        display:'flex',
+        flexDirection:'column',
+    },
+    title: {
+        color:'black',
+        fontWeight:'bold',
+        fontSize:'20px',
+        padding:'5px',
+        marginTop:'-70px'
+    },
+    text: {
+        color:'black',
+        fontSize:'15px',
+        textAlign:'center',
+        padding:'10px'
+    }
 })
 
 export default stylesConteudo;
